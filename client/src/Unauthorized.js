746674d5d32e8cf54e44a133195ce72aefee592c
Edwin Carbajal <exaviercarbajal@gmail.com>
2020-01-25 17:30:16 -0500

    Add login form to homepage

---
+++ client/src/Unauthorized.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Grid, Header } from 'semantic-ui-react'
+
+class Unauthorized extends React {
+  render() {
+    return(
+      <Grid centered columns={2}>
+        <Grid.Column>
+          <Header as='h1' textAlign='center'>
+            Before we get started, what's your name<br/>and email?
+            <Header.Subheader>
+              Create an account to track your progress.
+            </Header.Subheader>
+          </Header>
+          {this.props.children}
+        </Grid.Column>
+      </Grid>
+    )
+  }
+}
+export default Unauthorized;
--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
 import 'semantic-ui-css/semantic.min.css'
+import './index.css';
 import App from './App/App';
 
 render((
@@ -10,5 +11,3 @@ render((
         <App/>
     </BrowserRouter>
 ), document.getElementById('root'));
-
-//ReactDOM.render(<App />, document.getElementById('root'));
