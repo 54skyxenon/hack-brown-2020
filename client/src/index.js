bca8e645904d39a40279418e344d7364232c5fc5
Edwin Carbajal <exaviercarbajal@gmail.com>
2020-01-25 16:39:54 -0500

    Switch to npm and add semantic-ui

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,8 @@
-/*
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-*/
-
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
-import './index.css';
+import 'semantic-ui-css/semantic.min.css'
 import App from './App/App';
 
 render((
