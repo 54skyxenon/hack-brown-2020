538f90dd97cb098eb9992f314d97271f550d51cc
Brandon Liang <54skyxenon@gmail.com>
2020-01-25 17:12:36 -0500

    Initial frontend routing commit

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
+
 import Home from './pages/Home';
-import List from './pages/List';
+// import List from './pages/List';
+import Mission from './pages/Mission';
+import Community from './pages/Community';
+import Profile from './pages/Profile';
+import Login from './pages/Login';
 
 class App extends Component {
   render() {
@@ -10,7 +15,11 @@ class App extends Component {
       <div>
         <Switch>
           <Route exact path='/' component={Home}/>
-          <Route path='/list' component={List}/>
+          <Route path="/mission" component={Mission} />
+          <Route path="/community" component={Community} />
+          <Route path="/profile" component={Profile} />
+          <Route path="/login" component={Login} />
+          { /*<Route path='/list' component={List}/> */}
         </Switch>
       </div>
     )
--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,3 @@
-/*
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-*/
-
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
@@ -18,5 +10,3 @@ render((
         <App/>
     </BrowserRouter>
 ), document.getElementById('root'));
-
-//ReactDOM.render(<App />, document.getElementById('root'));
