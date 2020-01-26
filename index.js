001948d856ed7a20757202b425fd397ba04d0266
Brandon Liang <54skyxenon@gmail.com>
2020-01-25 23:57:27 -0500

    Got all of the DB/auth functions to work

--- index.js
+++ index.js
@@ -1,31 +1,90 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 
-// An api endpoint that returns a short list of items
-app.get('/api/getList', (req, res) => {
-    var list = ["item1", "item2", "item3"];
-    res.json(list);
-});
+app.use(cors())
+
+// DATABASE CONNECTION
+// Firebase App (the core Firebase SDK) is always required and must be listed before other Firebase SDKs
+var firebase = require("firebase/app");
+
+// Add the Firebase products that you want to use
+require("firebase/firestore");
+require("firebase/auth");
+
+const firebaseConfig = {
+    apiKey: "AIzaSyADHsTe60Lg8Bq87-kRO55Ari8jcIZqnNc",
+    authDomain: "corvus-74302.firebaseapp.com",
+    databaseURL: "https://corvus-74302.firebaseio.com",
+    projectId: "corvus-74302",
+    storageBucket: "corvus-74302.appspot.com",
+    messagingSenderId: "59563198527",
+    appId: "1:59563198527:web:a58ca5ff4048ce7dfa108a",
+    measurementId: "G-J01ZCEKNPM"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
 
 // AUTHENTICATION
-const passport = require("passport");
-const session = require("express-session");
-const LocalStrategy = require("passport-local").Strategy;
-
-app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
-app.use(passport.initialize());
-app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
-app.post("/login",
-    passport.authenticate("local", {
-        successRedirect: "/",
-        failureRedirect: "/"
-    })
-);
+app.post("/register", (req, res, next) => {
+    const { firstName, lastName, email, password, confirmPassword } = req.body;
+
+    if (!firstName || !lastName || !email || !password || !confirmPassword) {
+        // Are any fields blank?
+        res.send("Some fields were missing! <a href='/'>Go back?</a>")
+    } else if (password !== confirmPassword) {
+        // Do the passwords match?
+        res.send("Passwords don't match! <a href='/'>Go back?</a>");
+    } else {
+        // Everything look's good, create the user in auth and the DB...
+        firebase.auth()
+            .createUserWithEmailAndPassword(email, password)
+            .catch((err) => {
+                // Handle Errors here.
+                var errorCode = err.code;
+                var errorMessage = err.message;
+                res.send("Something went wrong... <a href='/'>Go back?</a>");
+            });
+
+        let docRef = db.collection('users').doc(email);
+
+        docRef.set({
+            firstName: firstName,
+            lastName: lastName,
+            email: email
+        });
+        res.redirect("/");
+    }
+});
+
+app.post("/login", (req, res, next) => {
+    firebase.auth()
+        .signInWithEmailAndPassword(req.body.email, req.body.password)
+        .then(() => {
+            res.send("Success!");
+        })
+        .catch(function (error) {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            res.send("Username or password is incorrect! <a href='/login'>Go back?</a>");
+        });
+});
+
+app.post("/logout", (req, res, next) => {
+    firebase.auth()
+        .signOut()
+        .then(() => {
+            res.redirect("/");
+        }).catch(err => {
+            res.redirect("/");
+        });
+});
 
 // Run the server
 const port = process.env.PORT || 5000;
 app.listen(port);
 console.log('Backend is listening on port ' + port + ' and frontend is listening on port 3000');
-
